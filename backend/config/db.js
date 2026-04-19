40ea94b60d0fca0edfa800a922edaf61fef3d9ab
Sruthi B <srubk2005@gmail.com>
2026-04-19 22:57:53 +0530

    fixed mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/financeDB");
-
-    console.log("Local MongoDB connected successfully");
+    await mongoose.connect("mongodb+srv://sruthi:Sruthi%402005@financeuser.v4mfhtu.mongodb.net/financeDB?retryWrites=true&w=majority");
+    console.log("MongoDB connected");
+    console.log("MONGO_URI:", process.env.MONGO_URI);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("Personal Finance Advisor API running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
